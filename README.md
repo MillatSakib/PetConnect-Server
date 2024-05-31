1399a67bc6d3aab4679930e2c3cfa030fc7561c0
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-05-31 23:58:48 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pet Adoption Server
+
+## Setup
+
+For initializing this project you have to use the command below:
+
+```sh
+npm i
+```
+
+It will install all package and dependency need for your project. But it is not enough for run this project properly. You have to add a `.env` file in your root.
+And the structure of the environment file are given below:
+
+```env
+DB_User="*********"
+DB_Pass="******"
+Access_Token_Secret=*************************
+```
+
+If you setup this project in this way The project will run properly.
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lm9a1gh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Pet is playing.');
+})
+
+
+async function run() {
+    try {
+        const userCollection = client.db("petAdoption").collection("users");
+
+        app.get('/demo', async (req, res) => {
+            res.status(200).send("Server Working Perfectly!")
+        });
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Pet is playing on port ${port}`);
+})
